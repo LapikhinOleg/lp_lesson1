92746060b8067c7595a7e5b84a0c91af5e8f9174
Oleg <lapikhin.oleg@gmail.com>
2024-01-25 11:01:49 +0800

    add lesson1:task6

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿double a = 5.5;
+int b = 2;
+int c = 6;
+double d = 1.75;
+double product = a * b;
+double sum = c + d;
+Console.WriteLine(product);
+Console.WriteLine(sum);
+
+double quotient = product / sum;
+Console.WriteLine(quotient);
